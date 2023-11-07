1ca5e6d08ceac85f93ec1c1cd651ff0ad20d40e5
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-06 20:36:45 -0500

    upload functionality online

--- routes/videos.js
+++ routes/videos.js
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
     const videos = readData();
     const video = videos.find((video) => video.id === id);
     if (!video) {
-    return res.status(404).json({ message: "No video with that id exists" });
+        return res.status(404).json({ message: "No video with that id exists" });
     }
     res.json(video);
 });
@@ -34,8 +34,29 @@ router.post('/', (req, res) => {
         id: uuidv4(),
         title,
         description,
-        channel,
-        image: '/public/images/upload-video-preview.jpg', 
+        channel: 'Christian Garcia',
+        image: 'http://localhost:8085/images/upload-video-preview.jpg',
+        views: "1,001,023",
+        likes: "110,985",
+        duration: "4:01",
+        video: "https://project-2-api.herokuapp.com/stream",
+        timestamp: 1626032763000,
+        comments: [
+            {
+                id: "35bba08b-1b51-4153-ba7e-6da76b5ec1b9",
+                name: "Micheal Lyons",
+                comment: "They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.",
+                likes: 0,
+                timestamp: 1628522461000
+            },
+            {
+                id: "091de676-61af-4ee6-90de-3a7a53af7521",
+                name: "Gary Wong",
+                comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+                likes: 0,
+                timestamp: 1626359541000
+            },
+        ]
     };
 
     videos.push(newVideo);
